56cb1779214e765f6b88e830afb24bc075ba0afd
Nikhil <nikhilsamuel.89@gmail.com>
2018-10-10 17:09:45 -0400

    Add todo items controller and actions

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,28 @@
+class TodoItemsController < ApplicationController
+  before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
+
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
+  end
+
+  def destroy
+    @todo_item.destroy
+    flash[:notice] = "Todo item successfully deleted"
+    redirect_to @todo_list
+  end
+
+  private
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
+  def todo_item_params
+    params.require(:todo_item).permit(:content)
+  end
+end
